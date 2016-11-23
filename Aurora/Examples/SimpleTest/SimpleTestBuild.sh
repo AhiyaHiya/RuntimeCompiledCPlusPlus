f203afab6431b28295e9dc818ce66e9223b433bd
Jaime O. Rios <online@jaimerios.com>
2016-11-22 20:16:05 -0500

    Moved functions out of build script so that they are accessible from Xcode project.

---
+++ Aurora/Examples/SimpleTest/SimpleTestBuild.sh
@@ -0,0 +1,60 @@
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+BuildXcodeProjectForTarget()
+{
+	printf "${FUNCNAME[0]}\n"
+	
+	local projectName=$1
+	local targetName=$2
+	local config=$3
+	local configPath=$4
+
+	xcodebuild \
+		-project ${projectName} \
+		-target ${targetName} \
+			-configuration ${config} \
+			CONFIGURATION_BUILD_DIR=${configPath} \
+			clean build | xcpretty
+	# if you want to use egrep instead of xcpretty
+	#| egrep -A 5 "(error|warning):"
+}
+
+SetupVariables()
+{
+	CONFIG=Debug
+	CURRENTPATH=${PWD}
+	cd ${ABSOLUTE_PATH}
+	BUILDPATH=${PWD}/xcbuild
+	cd ../..
+	BASEPATH=${PWD}
+}
+
+
+BuildFrameworks()
+{
+	cd ${BASEPATH}/Renderer
+	local frameworkBuildPath=${BUILDPATH}/Products/${CONFIG}/Frameworks
+	BuildXcodeProjectForTarget Renderer.xcodeproj Renderer ${CONFIG} ${frameworkBuildPath}
+		
+	cd ${BASEPATH}/RuntimeCompiler
+	BuildXcodeProjectForTarget RuntimeCompiler.xcodeproj RuntimeCompiler ${CONFIG} ${frameworkBuildPath}
+
+	cd ${BASEPATH}/RuntimeObjectSystem
+	BuildXcodeProjectForTarget RuntimeObjectSystem.xcodeproj RuntimeObjectSystem ${CONFIG} ${frameworkBuildPath}
+
+	cd ${BASEPATH}/Systems
+	BuildXcodeProjectForTarget Systems.xcodeproj Systems ${CONFIG} ${frameworkBuildPath}
+	
+	cd ${BASEPATH}/External/libRocket/Build
+	BuildXcodeProjectForTarget Rocket.xcodeproj RocketControlsOSX ${CONFIG} ${frameworkBuildPath}
+	BuildXcodeProjectForTarget Rocket.xcodeproj RocketDebuggerOSX ${CONFIG} ${frameworkBuildPath}
+}
+
+CopyFrameworksAndLibs()
+{
+	cd ${ABSOLUTE_PATH}
+	cp -R ${BUILDPATH}/Products/${CONFIG}/Frameworks ${BUILDPATH}/Products/${CONFIG}/SimpleTest.app/Contents/Frameworks
+}
